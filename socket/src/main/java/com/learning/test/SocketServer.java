4ead668729cedbb0d0f7acc9fb10a2f80210c396
飞 胡 <hu2670142525@163.com>
2022-04-27 11:05:05 +0800

    add spring file mapping

---
+++ socket/src/main/java/com/learning/test/SocketServer.java
@@ -0,0 +1,35 @@
+package com.learning.test;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @ClassName SocketServer
+ * @Description TODO
+ * @Author hufei
+ * @Date 2022/4/8 14:42
+ * @Version 1.0
+ */
+public class SocketServer {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(3000);
+            System.out.println("启动服务器....");
+            Socket s = ss.accept();
+            System.out.println("客户端:" + s.getInetAddress().getLocalHost() + "已连接到服务器");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            //读取客户端发送来的消息
+            String mess = br.readLine();
+            System.out.println("客户端：" + mess);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+            bw.write(mess + "\n");
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
